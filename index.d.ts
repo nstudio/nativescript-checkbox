b8fe25bc86366abfde9aebaf88f6d6b4a6620755
bradmartin <bradwaynemartin@gmail.com>
2016-08-09 22:55:07 -0500

    iOS & bump

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,35 @@
-export * from './checkbox';
+
+/**
+ * Represents a CheckBox component.
+ */
+export declare class CheckBox {
+
+    /**
+     * Gets the native [android widget](https://developer.android.com/reference/android/widget/CheckBox.html) that represents the user interface for this component. Valid only when running on Android OS.
+     */
+    android: any /* android.widget.CheckBox */;
+
+    /**
+    * Gets the ios Label with the checkbox as a subview
+    */
+    ios: any /* Label */;
+
+    /**
+     * Gets or sets if a switch is checked or not.
+     */
+    checked: boolean;
+
+    /**
+     * Change the checked state of the view to the inverse of its current state.
+     */
+    toggle(): void;
+
+}
+
+export interface CheckBoxInterface {
+    text: string;
+    checked: boolean;
+    fillColor: string;
+    tintColor: string;
+    toggle(): void;
+}
